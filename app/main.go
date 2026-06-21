07db841af2fd89e9f5311eb7e513e3b966e330f7
KrokaKrola <harlamsan@gmail.com>
2026-06-21 13:11:01 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,10 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 const (
 	exitCommand string = "exit"
+	echoCommand string = "echo"
 )
 
 func main() {
@@ -21,11 +23,29 @@ func main() {
 			break
 		}
 
-		cmd := scanner.Text()
+		line := scanner.Text()
+
+		splits := strings.Fields(line)
+
+		if len(splits) == 0 {
+			fmt.Fprintln(os.Stderr, "invalid number of arguments")
+			break
+		}
+
+		cmd := splits[0]
 
 		switch cmd {
 		case exitCommand:
 			os.Exit(0)
+		case echoCommand:
+			if len(splits) < 2 {
+				fmt.Println()
+				break
+			}
+
+			args := splits[1:]
+
+			fmt.Println(strings.Join(args, " "))
 		default:
 			fmt.Fprintf(writer, "%s: command not found\n", cmd)
 		}
